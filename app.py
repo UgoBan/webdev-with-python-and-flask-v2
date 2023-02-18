1aeb30776a8b050eaf5f58b9bd17579ab74ddf5d
Banabas Ejiofor <111522556+UgoBan@users.noreply.github.com>
2023-02-18 12:45:15 +0000

    added dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Regulatory Ecotoxicologist',
+    'location': 'Berlin, Germany',
+    'salary': 'EUR. 55,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Frankfurt, Germany',
+    'salary': 'EUR. 50,000'
+  },
+  {
+    'id': 3,
+    'title': 'Data Analyst',
+    'location': 'Munich, Germany'
+  },
+  {
+    'id': 4,
+    'title': 'Frontend Developer',
+    'location': 'Hamburg, Germany',
+    'salary': 'EUR. ,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs = JOBS, company_name = 'Banabas')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug = True)
